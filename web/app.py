8f14f291ce1e1234fd3e91377ca3133a5549c190
EC2 Default User <ec2-user@ip-172-31-31-45.eu-central-1.compute.internal>
2018-04-26 15:11:58 +0000

    dodano wrzucanie zdjec

--- web/app.py
+++ web/app.py
@@ -6,12 +6,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-  return "here will be my upload interface"
+  return render_template('upload_files.html')
 
 @app.route("/make-animation")
 def make_animation():
   return render_template("make_animation.html", invitaion="only limit is yourself")
 
+@app.route("/upload")
+def handle_upload():
+  return OK
 
 if __name__ == '__main__':
   app.run(host="0.0.0.0", port=8080, debug=True)
